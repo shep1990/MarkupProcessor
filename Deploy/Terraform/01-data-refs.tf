8e76a34ab8031734dafd14bf68b7f1789c02dff8
Richard Shepherd <richardjohn90@hotmail.co.uk>
2022-12-29 20:25:28 +0000

    Added cosmos db terraform

--- Deploy/Terraform/01-data-refs.tf
+++ Deploy/Terraform/01-data-refs.tf
@@ -1,3 +1,8 @@
 data "azurerm_resource_group" "markupProcessor-rg" {
   name  = "MarkupProcessor-RG"
 }
+
+data "azurerm_cosmosdb_account" "markupProcessor-cosmosdb-account" {
+  name                = "cosmosdb"
+  resource_group_name = data.azurerm_resource_group.markupProcessor-rg.name
+}
