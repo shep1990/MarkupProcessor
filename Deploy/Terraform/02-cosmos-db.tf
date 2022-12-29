8ad44f40bda2ad6503476325899d6588a6ebaa30
Richard Shepherd <richardjohn90@hotmail.co.uk>
2022-12-29 23:03:47 +0000

    updated terraform to propery reference rg

--- Deploy/Terraform/02-cosmos-db.tf
+++ Deploy/Terraform/02-cosmos-db.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cosmosdb_sql_database" "markup-processor-database" {
   name                = "MarkupProcessor"
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.markup-processor-rg.name
   account_name        = var.cosmos_db_name
 }
