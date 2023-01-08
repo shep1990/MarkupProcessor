4950cb91252f17a3107bd3baa7a703f11d4dffca
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-01-08 14:42:02 +0000

    Added in other req fields

--- Deploy/Terraform/02-cosmos-db.tf
+++ Deploy/Terraform/02-cosmos-db.tf
@@ -2,6 +2,16 @@ resource "azurerm_cosmosdb_account" "markup-processor-account" {
   name                = var.cosmos_db_name
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.markup-processor-rg.name
+  offer_type          = "Standard"
+    consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+    geo_location {
+    location          = "westeu"
+    failover_priority = 0
+  }
 }
 
 
