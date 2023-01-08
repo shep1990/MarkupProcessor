9029b65cd6dfcd834c679f17643e8affd5092d87
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-01-08 14:26:54 +0000

    Added closing brace

--- Deploy/Terraform/02-cosmos-db.tf
+++ Deploy/Terraform/02-cosmos-db.tf
@@ -2,6 +2,7 @@ resource "azurerm_cosmosdb_account" "markup-processor-account" {
   name                = var.cosmos_db_name
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.markup-processor-rg.name
+}
 
 
 resource "azurerm_cosmosdb_sql_database" "markup-processor-database" {
