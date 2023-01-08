05f04b81a01c7d093cb5bde1a859e7e1f1b313a7
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-01-08 16:01:08 +0000

    slight update to app service terraform

--- Deploy/Terraform/03-app-service.tf
+++ Deploy/Terraform/03-app-service.tf
@@ -2,11 +2,11 @@ resource "azurerm_service_plan" "personal_projects" {
   name                = "personal_app_service_plan"
   resource_group_name = azurerm_resource_group.markup-processor-rg.name
   location            = azurerm_resource_group.markup-processor-rg.location
-  os_type             = "Windows"
+  os_type             = "Linux"
   sku_name            = "F1"
 }
 
-resource "azurerm_windows_web_app" "markup_app" {
+resource "azurerm_linux_web_app" "markup_app" {
   name                = "markup-processor-app"
   resource_group_name = azurerm_resource_group.markup-processor-rg.name
   location            = azurerm_service_plan.personal_projects.location
@@ -15,10 +15,8 @@ resource "azurerm_windows_web_app" "markup_app" {
   site_config {}
 }
 
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_windows_web_app.markup_app.id
-  repo_url           = "https://github.com/shep1990/MarkupProcessor"
-  branch             = "master"
-  use_manual_integration = true
-  use_mercurial      = false
+resource "azurerm_app_service_source_control" "source_control" {
+  app_id   = azurerm_linux_web_app.markup_app.id
+  repo_url = "https://github.com/shep1990/MarkupProcessor"
+  branch   = "master"
 }
