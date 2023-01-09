752697fb00e2aed67373d156f21b3719934fbf3f
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-01-09 13:35:56 +0000

    slight change

--- MarkupProcessor/Program.cs
+++ MarkupProcessor/Program.cs
@@ -3,9 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddSwaggerGen(options => 
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MarkupProcessor API", Version = "v1" })
-);
+builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
@@ -13,10 +11,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "MarkupProcessor API");
-});
+app.UseSwaggerUI();
 
 
 
