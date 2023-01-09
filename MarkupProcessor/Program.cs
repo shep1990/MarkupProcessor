987d6bab3affe5c8c5d22536cd98f03f2c5ca6ab
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-01-09 13:55:20 +0000

    removed swagger for now

--- MarkupProcessor/Program.cs
+++ MarkupProcessor/Program.cs
@@ -3,12 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MarkupProcessor API", Version = "v1" });
-    options.DocInclusionPredicate((docName, description) => true);
-    options.CustomSchemaIds(x => x.FullName);
-});
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
@@ -29,16 +23,6 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html"); ;
-
-// Configure the HTTP request pipeline.
-
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "MarkupProcessor API v1");
-});
-
-
 
 app.MapHealthChecks("/health");
 
