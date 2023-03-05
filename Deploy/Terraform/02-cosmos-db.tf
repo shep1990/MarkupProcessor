d5c9975a0271aeac04e30c6a97faaa9a92367c0e
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-03-05 19:07:51 +0000

    added back in db account creation

--- Deploy/Terraform/02-cosmos-db.tf
+++ Deploy/Terraform/02-cosmos-db.tf
@@ -1,13 +1,30 @@
+resource "azurerm_cosmosdb_account" "markup-processor-account" {
+  name                = var.cosmos_db_name
+  location            = var.resource_group_location
+  resource_group_name = data.azurerm_resource_group.markup-processor-rg.name
+  offer_type          = "Standard"
+    consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+    geo_location {
+    location          = var.resource_group_location
+    failover_priority = 0
+  }
+}
+
+
 resource "azurerm_cosmosdb_sql_database" "markup-processor-service-db" {
   name                = "markup-processor-database"
   resource_group_name = data.azurerm_resource_group.markup-processor-rg.name
-  account_name        = data.azurerm_cosmosdb_account.markup-processor-cosmosdb-account.name
+  account_name        = azurerm_cosmosdb_account.markup-processor-account.name
 }
 
 resource "azurerm_cosmosdb_sql_container" "markup-processor" {
   name                  = "markup-processor-container"
   resource_group_name   = data.azurerm_resource_group.markup-processor-rg.name
-  account_name          = data.azurerm_cosmosdb_account.markup-processor-cosmosdb-account.name
+  account_name          = azurerm_cosmosdb_account.markup-processor-account.name
   database_name         = azurerm_cosmosdb_sql_database.markup-processor-service-db.name
   partition_key_path    = "/flowChartId"
   partition_key_version = 1
--- Deploy/Terraform/data-refs.tf
+++ Deploy/Terraform/data-refs.tf
@@ -1,8 +1,3 @@
 data "azurerm_resource_group" "markup-processor-rg" {
   name     = "MarkupProcessor-RG"
 }
-
-data "azurerm_cosmosdb_account" "markup-processor-cosmosdb-account" {
-  name                = "${var.cosmos_db_name}"
-  resource_group_name = data.azurerm_resource_group.markup-processor-rg.name
-}
